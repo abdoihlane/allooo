1c307b9e00fac81e7d4ed5cfb5704ef10d680fbb
abdoihlane <habibiihlanea1@@gmail.com>
2025-07-24 20:57:18 +0100

    yoo

---
+++ final_minishill/jjjjjj/parsing.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahabibi- <ahabibi-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 02:46:06 by ahabibi-          #+#    #+#             */
+/*   Updated: 2025/07/24 19:54:57 by ahabibi-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	check_plus(char *sa)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (sa[i])
+	{
+		if (is_redirection(sa[i]))
+		{
+			j = i;
+			while (is_whitespace(sa[j]))
+				j++;
+			if (is_redirection(sa[j]))
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	hardcodechecks(char *str)
+{
+	int	i;
+	int	dquote;
+
+	if (!str)
+		return (0);
+	i = 0;
+	dquote = check_quotes_closed(str);
+	if (dquote == 1)
+		return (0);
+	if (red_check(str) == 0 || str[0] == '>' || check_plus(str) == 0)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '|')
+		{
+			if (str[i + 1] == '|')
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	red_check(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && i && str[i] <= 32)
+		i--;
+	if (str[0] == '|' || str[i] == '|')
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '<' && str[i + 1] == '<' && str[i + 2] == '<')
+			return (0);
+		if (str[i] == '<' && str[i + 1] == '>')
+			return (0);
+		if (str[i] == '>' && str[i + 1] == '<')
+			return (0);
+		if ((str[i] == '>') && (str[i + 1] == '>') && (str[i + 2] == '>'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
